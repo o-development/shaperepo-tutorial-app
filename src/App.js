a3fdd6ba9a4be4286c70e45df63ff9dedd516051
Jackson Morgan <jackson@MacBook-Pro.local>
2020-07-23 18:55:38 -0400

    Setup basic login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import auth from 'solid-auth-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Chat extends React.Component {
+  render() {
+    return (
+      <h1>Chat</h1>
+    )
+  }
 }
 
-export default App;
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputVal: ""
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Log In</h1>
+        <form onSubmit={async (e) => {
+          e.preventDefault()
+          auth.login(this.state.inputVal)
+        }}>
+          <label for="idpInput">Your IDP:</label>
+          <input
+            type="text"
+            placeholder="Example: https://solid.community"
+            onChange={(e) => this.setState({ inputVal: e.target.value })}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }
+}
+
+
+class ChatApp extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      loadingAuth: true,
+      session: null
+    }
+    // Check to see if already authenticated
+    auth.trackSession(session => {
+      console.log(session)
+      if (!session)
+        this.setState({ loadingAuth: false })
+      else
+        this.setState({ session: session, loadingAuth: false })
+    })
+  }
+
+  render() {
+    if (this.state.loadingAuth) {
+      return <h1>Loading...</h1>
+    } else if (!this.state.session) {
+      return <Login />
+    } else {
+      return (
+        <div>
+          <p>Logged in as {this.state.session.webId}</p>
+          <Chat />
+        </div>
+      )
+    }
+  }
+}
+
+export default ChatApp;
